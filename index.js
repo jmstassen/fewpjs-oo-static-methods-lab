94483194cdb35937ebdd63bf5e98399f7ebbd015
John M. Stassen <john.stassen@gmail.com>
2021-08-07 20:29:13 -0700

    commit tree f5a9662b914aa0402217e912910525c43651dac1

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string) {
+    const allowed = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const splitString = string.split(" ")
+    let titleArray = []
+
+    for (let i = 0; i < splitString.length; i++) {
+      if (i === 0) {
+        titleArray.push(this.capitalize(splitString[i]))
+      } else {
+        if (allowed.includes(splitString[i])) {
+          titleArray.push(splitString[i])
+        } else {
+          titleArray.push(this.capitalize(splitString[i]))
+        }
+      }
+    }
+    return titleArray.join(" ")
+  }
 }
